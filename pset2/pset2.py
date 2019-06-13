667a4f5bdd955e5cb05cc9d2ca8eddcf763b61a1
masterseeker <juldan01@googlemail.com>
2019-06-13 06:07:54 +0100

    Add files via upload

--- pset2/pset2.py
+++ pset2/pset2.py
@@ -1,62 +1,62 @@
-# -*- coding: utf-8 -*-
-#"""
-#Created on Thu Sep 13 19:47:51 2018
+ -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 13 19:47:51 2018
+
+@author: Tech
+"""
+
+"""part 1  just the minimum payment 
+
+balance
+annualInterestRate
+monthlyPaymentRate
+
+Monthly interest rate= (Annual interest rate) / 12.0
+Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
+Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
+Updated balance = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
+
+"""
 #
-#@author: Tech
-#"""
-#
-#"""part 1  just the minimum payment 
-#
-#balance
-#annualInterestRate
-#monthlyPaymentRate
-#
-#Monthly interest rate= (Annual interest rate) / 12.0
-#Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
-#Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
-#Updated balance = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
-#
-#"""
-##
-##balance = 42
-##annualInterestRate = 0.2
-##monthlyPaymentRate = 0.04
-##
-#monthlyInterestRate = annualInterestRate / 12.0 
-#unpaidBalance = balance 
-#
-#for month in range(12):
-#    minPayment = unpaidBalance * monthlyPaymentRate
-#    newUnpaidBalance = unpaidBalance - minPayment
-#    unpaidBalance = newUnpaidBalance +  (newUnpaidBalance * monthlyInterestRate)
-#print("Remaining balance: {0:.2f}".format(unpaidBalance) )
-#
-#
-#""" part 2 Lowest monthlty payment mutiple of 10"""
-#
-#
-##balance = 3926
-##annualInterestRate = 0.2
-#
-#monthlyInterestRate = annualInterestRate / 12.0 
-#payment = 10
-#BALANCE = balance
-#
-#while True: 
-#    unpaidBalance = BALANCE
-#    for month in range(12):
-#        newUnpaidBalance = unpaidBalance - payment
-#        unpaidBalance = newUnpaidBalance +  (newUnpaidBalance * monthlyInterestRate)
-#    if unpaidBalance <= 0: break
-#    payment += 10
-#
-#print('Lowest Payment: {}'.format(payment))
-#        
+#balance = 42
+#annualInterestRate = 0.2
+#monthlyPaymentRate = 0.04
+
+monthlyInterestRate = annualInterestRate / 12.0 
+unpaidBalance = balance 
+
+for month in range(12):
+    minPayment = unpaidBalance * monthlyPaymentRate
+    newUnpaidBalance = unpaidBalance - minPayment
+    unpaidBalance = newUnpaidBalance +  (newUnpaidBalance * monthlyInterestRate)
+print("Remaining balance: {0:.2f}".format(unpaidBalance) )
+
+
+""" part 2 Lowest monthlty payment mutiple of 10"""
+
+
+#balance = 3926
+#annualInterestRate = 0.2
+
+monthlyInterestRate = annualInterestRate / 12.0 
+payment = 10
+BALANCE = balance
+
+while True: 
+    unpaidBalance = BALANCE
+    for month in range(12):
+        newUnpaidBalance = unpaidBalance - payment
+        unpaidBalance = newUnpaidBalance +  (newUnpaidBalance * monthlyInterestRate)
+    if unpaidBalance <= 0: break
+    payment += 10
+
+print('Lowest Payment: {}'.format(payment))
+        
 
 """ part 3 lowest payment to the penny using bi-section search """
 
-balance = 999999
-annualInterestRate = 0.18
+#balance = 999999
+#annualInterestRate = 0.18
 
 monthlyInterestRate = annualInterestRate / 12.0
 lowerBound = balance / 12
